44e4e7345d3c31a5a2c9a4a010462a862787463c
Allan Højgaard Jensen <ahj@nykredit.dk>
2015-12-03 18:03:07 +0100

    moved karma config from root to tasks

---
+++ tasks/karma.conf.js
@@ -0,0 +1,29 @@
+/*global module */
+
+module.exports = function (config) {
+    'use strict';
+    config.set({
+        basePath: '../',
+        frameworks: ['jasmine'],
+        files: ['**/*/*_test.js'],
+        exclude: ['**/bower_components'],
+        preprocessors: {
+            'app/!(bower_components)/!(patch)/**/!(*_test).js': ['coverage']
+        },
+        reporters: ['progress', 'junit', 'coverage'],
+        junitReporter: {
+            outputFile: 'target/test-results.xml'
+        },
+        coverageReporter: {
+            type: 'cobertura',
+            dir: 'target/coverage/'
+        },
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['PhantomJS'],
+        captureTimeout: 60000,
+        singleRun: false
+    });
+};
