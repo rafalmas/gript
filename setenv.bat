9ae215ffc943ca31621a8c05d5895466539fcba6
Allan Højgaard Jensen <ahj@nykredit.dk>
2015-12-14 18:39:26 +0100

    added dev and dist license tools

--- setenv.bat
+++ setenv.bat
@@ -1,10 +1,15 @@
 call cfg nodejs 4.2.2 git 1.9.5.msysgit.0 ruby 2.2.2 python 2.7.10
 
 REM Fetch npm dependencies
-set PATH=%PATH%;%PRO_ROOT%\node_modules\.bin
+set PATH=%PATH%;%cd%\node_modules\.bin
 set HTTP_PROXY=http://webproxy:8080
 git config --global url."https://".insteadOf git://
 call npm install
 
-EM Ensure that Protractor browser support is available
+set _licenses=target\licenses
+md %_licenses%
+call license-checker --json > %_licenses%/dev-npm-licenses.json
+call bower-license > %_licenses%/app-bower-licenses.json
+
+REM Ensure that Protractor browser support is available
 call node_modules\.bin\webdriver-manager.cmd update
