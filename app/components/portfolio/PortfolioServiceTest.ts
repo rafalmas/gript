b7717fcc1f33f97deba4e8449d0e2b5b1db0553d
Jarek Krochmalski <jarek@finsys.pl>
2016-02-01 13:29:47 +0100

    test fixed

--- app/components/portfolio/PortfolioServiceTest.ts
+++ app/components/portfolio/PortfolioServiceTest.ts
@@ -19,7 +19,7 @@ describe('PortfolioService', () => {
     it('should return portfolio', function () {
         var portfolio = service.getPortfolio();
         expect(portfolio.length).toBe(8);
-        expect(portfolio).toContain({name: 'Kids', balance: 200348.76, type: 'ca', remark: 'Closed saving Account for kids'});
+        expect(portfolio).toContain({name: 'Kids', balance: 200348.76, type: 'kids', remark: 'Closed saving Account for kids'});
         expect(portfolio).toContain({name: 'Savings', balance: 220454.54, type: 'sa', remark: 'Family Savings'});
     });
 });
