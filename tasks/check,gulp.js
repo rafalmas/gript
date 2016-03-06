c1a7a8919b6d1f56d55a2a8ab907c6ea256b9544
Jarek Krochmalski <jarek@finsys.pl>
2016-03-06 10:19:06 +0100

    fixed linter error

--- tasks/check,gulp.js
+++ tasks/check,gulp.js
@@ -6,7 +6,9 @@ module.exports = function (gulp) {
 
     function check(element) {
         if (!_.has(gulp.config, element)) {
-            util.log('Your config is missing the mandatory ' + util.colors.red(element) + ' value. Refer to the readme.md and check the gulpfile.js');
+            util.log('Your config is missing the mandatory '
+                + util.colors.red(element)
+                + ' value. Refer to the readme.md and check the gulpfile.js');
             process.exit(1);
         }
     }
